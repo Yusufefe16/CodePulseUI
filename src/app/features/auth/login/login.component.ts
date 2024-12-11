c89533ec894d99c10d7372b34e4351c92f222feb
Yusuf <yefe1538@gmail.com>
2024-12-12 01:01:21 +0300

    created auth service

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {LoginRequest} from '../models/login-request.model';
+import {AuthService} from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,9 @@ export class LoginComponent {
 
   model: LoginRequest;
 
-  constructor() {
+  constructor(
+    private authService: AuthService
+  ) {
     this.model = {
       email: '',
       password: ''
@@ -19,5 +22,13 @@ export class LoginComponent {
 
   onFormSubmit(): void {
     console.log(this.model)
+    this.authService.login(this.model).subscribe({
+      next: (res)=>{
+        console.log(res)
+      },
+      error: (err) =>{
+        console.log(err)
+      }
+    })
   }
 }
